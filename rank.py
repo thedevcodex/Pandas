c11579eeebf0568a2e4b1a17cf11074ccaa9882d
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-17 06:35:12 +0530

    Sort ranked products by total revenue

--- rank.py
+++ rank.py
@@ -2,3 +2,4 @@
 total_revnue=df.groupby('product_name')['total_amount'].sum().reset_index()
 total_revnue
 total_revnue["Rank"]=total_revnue['total_amount'].rank(ascending=False).astype(int)
+total_revnue.sort_values("Rank",ascending=True)
